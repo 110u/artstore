c8410c9ead0dddc024889c539812b75d04d2bcc2
本名 <futp3249@hotmail.com>
2016-03-24 18:58:20 +0800

    Fix

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -8,7 +8,7 @@ CarrierWave.configure do |config|
 
       aws_secret_access_key:     ENV['aws_secret_access_key'],   # 你的 secret key
 
-      region:  ENV['ap-northeast-1'] # 你的 S3 bucket 的 Region 位置
+      region:  ENV['region'] # 你的 S3 bucket 的 Region 位置
 
     }
     config.fog_directory  = 'gundiyu'# 你設定的 bucket name
